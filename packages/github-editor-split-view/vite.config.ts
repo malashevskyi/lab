5eade3871f725a56b82f162ffee8267df2c5d8b0
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-10-25 09:12:49 +0300

    chore: react setup

---
+++ packages/github-editor-split-view/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        content: 'src/content.tsx',
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+    minify: false,
+  },
+  publicDir: 'public',
+});

fcd43184f4b6204a8dc71c5e0fb0bc03025cea11
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-10-28 21:52:26 +0200

    feat: add flashcard entity

---
+++ packages/server/src/flashcards/entities/flashcard.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import type {
+  FlashcardType,
+  KnowledgeLevel,
+  UsageContext,
+} from '@lap/types/deep-read/flashcards/index.js';
+
+@Entity('flashcards')
+export class FlashcardEntity implements FlashcardType {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'text' })
+  question: string;
+
+  @Column({ type: 'text' })
+  answer: string;
+
+  @Column({ name: 'source_url', type: 'text', nullable: true })
+  sourceUrl: string;
+
+  @Column({ type: 'text', array: true, default: '{}' })
+  tags: string[];
+
+  @Column({ type: 'varchar' })
+  level: KnowledgeLevel;
+
+  @Column({ type: 'varchar', array: true, default: '{}' })
+  contexts: UsageContext[];
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: string;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt: string;
+}

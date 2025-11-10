de3b6f20f66fed009c499988d7e29908d8dbd877
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-11-10 22:37:31 +0200

    feat: add highlight theme support for snippets

--- packages/vs-code-code-flashcards/src/flashcardViewProvider.ts
+++ packages/vs-code-code-flashcards/src/flashcardViewProvider.ts
@@ -3,12 +3,46 @@ import * as fs from 'fs';
 import * as path from 'path';
 import MarkdownIt from 'markdown-it';
 import { CodeSnippet } from './types';
+import mdhljs from 'markdown-it-highlightjs';
+
+import hljs from 'highlight.js/lib/core';
+
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+// import python from 'highlight.js/lib/languages/python';
+import sql from 'highlight.js/lib/languages/sql';
+import css from 'highlight.js/lib/languages/css';
+import scss from 'highlight.js/lib/languages/scss';
+// import javahtml from 'highlight.js/lib/languages/xml'; // for HTML
+import json from 'highlight.js/lib/languages/json';
+// import rust from 'highlight.js/lib/languages/rust';
+// import go from 'highlight.js/lib/languages/go';
+// import java from 'highlight.js/lib/languages/java';
+// import php from 'highlight.js/lib/languages/php';
+// import ruby from 'highlight.js/lib/languages/ruby';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('typescript', typescript);
+// hljs.registerLanguage('python', python);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('scss', scss);
+// hljs.registerLanguage('html', javahtml);
+hljs.registerLanguage('json', json);
+// hljs.registerLanguage('rust', rust);
+// hljs.registerLanguage('go', go);
+// hljs.registerLanguage('java', java);
+// hljs.registerLanguage('php', php);
+// hljs.registerLanguage('ruby', ruby);
+// For TSX/JSX, typescript and javascript highlighters are often sufficient
+hljs.registerLanguage('tsx', typescript);
+hljs.registerLanguage('jsx', javascript);
 
 export class FlashcardViewProvider implements vscode.WebviewViewProvider {
   public static readonly viewType = 'code-flashcards.flashcardView';
 
   private _view?: vscode.WebviewView;
-  private md = new MarkdownIt({ html: true });
+  private md = new MarkdownIt({ html: true }).use(mdhljs, { hljs });
 
   private readonly _onDidReceiveMessage = new vscode.EventEmitter<any>();
   public readonly onDidReceiveMessage: vscode.Event<any> =
@@ -35,6 +69,12 @@ export class FlashcardViewProvider implements vscode.WebviewViewProvider {
 
   public getView(): vscode.WebviewView | undefined {
     return this._view;
+  }
+
+  public signalOperationComplete() {
+    if (this._view) {
+      this._view.webview.postMessage({ command: 'operationComplete' });
+    }
   }
 
   public update(snippets: CodeSnippet[], language: string) {
@@ -71,9 +111,14 @@ export class FlashcardViewProvider implements vscode.WebviewViewProvider {
     const scriptUri = webview.asWebviewUri(
       vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js')
     );
+    const hljsThemeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, 'media', 'hljs-theme.css')
+    );
+
     html = html.replaceAll('{{cspSource}}', webview.cspSource);
     html = html.replaceAll('{{styleUri}}', styleUri.toString());
     html = html.replaceAll('{{scriptUri}}', scriptUri.toString());
+    html = html.replaceAll('{{hljsThemeUri}}', hljsThemeUri.toString());
     return html;
   }
 }

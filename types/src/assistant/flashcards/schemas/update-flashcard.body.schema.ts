9b1a747e5c93af3de8b749c9425450d907a2f2fb
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-11-19 22:01:49 +0200

    types: make flashcard context editable

--- types/src/assistant/flashcards/schemas/update-flashcard.body.schema.ts
+++ types/src/assistant/flashcards/schemas/update-flashcard.body.schema.ts
@@ -1,9 +1,12 @@
 import { z } from 'zod';
 import { flashcardTypeSchema } from './flashcard.schema.js';
 
-export const updateFlashcardBodySchema = flashcardTypeSchema.pick({
-  question: true,
-  answer: true,
-});
+export const updateFlashcardBodySchema = flashcardTypeSchema
+  .pick({
+    question: true,
+    answer: true,
+    context: true,
+  })
+  .partial();
 
 export type UpdateFlashcardBodyType = z.infer<typeof updateFlashcardBodySchema>;

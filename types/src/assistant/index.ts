ebf3c3e63519d40d001579819817040e87aa965e
Vitalii Malashevskyi <shevsky1488@gmail.com>
2025-12-17 20:50:05 +0200

    refactor(prettier): double quotes

--- types/src/assistant/index.ts
+++ types/src/assistant/index.ts
@@ -1,8 +1,8 @@
-export * as ai from './ai/index.js';
-export * as audioRecords from './audio-records/index.js';
-export * as dictionaryEntries from './dictionary-entries/index.js';
-export * as dictionaryExamples from './dictionary-examples/index.js';
-export * as tts from './tts/index.js';
-export * as flashcards from './flashcards/index.js';
-export * as stack from './stack/index.js';
-export * as chunks from './chunks/index.js';
+export * as ai from "./ai/index.js";
+export * as audioRecords from "./audio-records/index.js";
+export * as dictionaryEntries from "./dictionary-entries/index.js";
+export * as dictionaryExamples from "./dictionary-examples/index.js";
+export * as tts from "./tts/index.js";
+export * as flashcards from "./flashcards/index.js";
+export * as stack from "./stack/index.js";
+export * as chunks from "./chunks/index.js";
